0f414e32eea0a89242982300b098abb97e987779
yutacommits <you@example.com>
2025-12-01 03:39:12 -0600

    Added server to Makefile, and started some clean up. More logs.

--- beej/src/server.c
+++ beej/src/server.c
@@ -15,9 +15,10 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3490"  // the port users will be connecting to
-
-#define BACKLOG 10   // how many pending connections queue will hold
+// the port users will be connecting to
+#define PORT "3490"
+// how many pending connections queue will hold
+#define BACKLOG 10
 
 void sigchld_handler(int s)
 {
@@ -47,47 +48,69 @@ int main(void)
     // listen on sock_fd, new connection on new_fd
     int sockfd, new_fd;
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address info
+    struct sockaddr_storage their_addr;
     socklen_t sin_size;
     struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
+    int addr_reuse = 1;
+    char ip_str[INET6_ADDRSTRLEN];
     int rv;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
+    // bind to the first address we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
+        struct in_addr *ipv4;
+        struct in6_addr *ipv6;
+
+        if (p->ai_family == AF_INET)
+        {
+            ipv4 = &(((struct sockaddr_in *)p->ai_addr)->sin_addr);
+            inet_ntop(p->ai_family, ipv4, ip_str, sizeof ip_str);
+        }
+        else if (p->ai_family == AF_INET6)
+        {
+            ipv6 = &(((struct sockaddr_in6 *)p->ai_addr)->sin6_addr);
+            inet_ntop(p->ai_family, ipv6, ip_str, sizeof ip_str);
+        }
+        else
+        {
+            printf("[-] Unable to determine address family\n");
+            exit(1);
+        }
+        printf("[+] Using address %s\n", ip_str);
+
+        printf("Creating socket.\n");
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
-            perror("server: socket");
+            perror("Failed to create socket.\n");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+        printf("Setting address to be reuseable\n");
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &addr_reuse,
                 sizeof(int)) == -1) {
-            perror("setsockopt");
+            perror("Failed to set socket to be reuseable.");
             exit(1);
         }
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
-            perror("server: bind");
+            perror("Failed to bind socket.");
             continue;
         }
 
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo);
 
     if (p == NULL)  {
         fprintf(stderr, "server: failed to bind\n");
@@ -99,7 +122,7 @@ int main(void)
         exit(1);
     }
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
@@ -107,9 +130,8 @@ int main(void)
         exit(1);
     }
 
-    printf("server: waiting for connections...\n");
-
-    while(1) {  // main accept() loop
+    printf("[+] Waiting for connections...\n");
+    while(1) {
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
                 &sin_size);
@@ -120,8 +142,8 @@ int main(void)
 
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
+            ip_str, sizeof ip_str);
+        printf("server: got connection from %s\n", ip_str);
 
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
